a8ecf5917b8ad5dd8175f443ca7eca5ae65cf5d8
Bashtov <82177146+Bashtov@users.noreply.github.com>
2021-04-15 15:48:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+(function() {
+  const menuburger =document.querySellector('.menuburger');
+  menu__burger.addEventListener ('click', () => {menu__burger.classList.toggle('menuburger_active')
+  })
+}())
